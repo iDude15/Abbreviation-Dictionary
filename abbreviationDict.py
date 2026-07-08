b6a803cbf145cda36ca27d711903178dd476c344
iDude15 <44753452+iDude15@users.noreply.github.com>
2026-07-08 19:27:45 -0400

    Create abbreviationDict.py

---
+++ abbreviationDict.py
@@ -0,0 +1,91 @@
+# Abbreviation Dictionary by i-MCDude
+# This program defines abbreviations as you type the letters
+
+import sys
+
+print('Welcome to the Abbreviation Dictionary! (This program is a work in progress, which means more abbreviations are added soon!)')
+print('Enter an abbreviation, and I will expand it for you, type list to view available abbreviations, or type q to quit')
+print('Remember, it has to be in caps!')
+print('Made in Python 3!')
+while True:
+	abbreviation = input('Enter abbreviation: ') # User has to type an abbreviation of their choosing
+	if abbreviation == 'VIP':
+			print("Very Important Person")
+	elif abbreviation == 'ASAP':
+			print("As Soon As Possible")
+	elif abbreviation == 'NASA':
+			print("National Aeronautics and Space Administration")
+	elif abbreviation == 'NASCAR':
+			print("National Association for Stock Car Auto Racing")
+	elif abbreviation == 'WWW':
+			print("World Wide Web")
+	elif abbreviation == 'USB':
+			print("Universal Serial Bus")
+	elif abbreviation == 'RAM':
+			print("Random Access Memory")
+	elif abbreviation == 'CPU':
+			print("Central Processing Unit")
+	elif abbreviation == 'SOS':
+			print("Save Our Ship")
+	elif abbreviation == 'EPA':
+			print("Environmental Protection Agency")
+	elif abbreviation == 'LOL':
+			print("Laugh Out Loud")
+	elif abbreviation == 'VR':
+			print("Virtual Reality")
+	elif abbreviation == 'PTSD':
+			print("Post Traumatic Stress Disorder")
+	elif abbreviation == 'CD':
+			print("Compact Disk")
+	elif abbreviation == 'EST':
+			print("Eastern Standard Time")
+	elif abbreviation == 'AST':
+			print("Atlantic Standard Time")
+	elif abbreviation == 'MST':
+			print("Mountain Standard Time")
+	elif abbreviation == 'ADHD':
+			print("Attention-Deficit Hyperactivity Disorder")
+	elif abbreviation == 'AA':
+			print("American Airlines or Alcoholics Anonymous")
+	elif abbreviation == 'NES':
+			print("Nintendo Entertainment System")
+	elif abbreviation == 'SNES':
+			print("Super Nintendo Entertainment System")
+	elif abbreviation == 'NDS':
+			print("Nintendo DS")
+	elif abbreviation == 'MB':
+			print("Megabyte")
+	elif abbreviation == 'TB':
+			print("Terabyte")
+	elif abbreviation == 'GB':
+			print("Gigabyte")
+	elif abbreviation == 'GBC':
+			print("Game Boy Color")
+	elif abbreviation == 'ROFL':
+			print("Rolling On the Floor Laughing")
+	elif abbreviation == 'LCD':
+			print("Liquid Crystal Display")
+	elif abbreviation == 'NFT':
+			print("Non-Fungible Token")
+	elif abbreviation == 'AI':
+			print("Artificial Intelligence")
+	elif abbreviation == 'WTF':
+			print("What the F@#!")
+	elif abbreviation == 'MLB':
+			print("Major League Baseball")
+	elif abbreviation == 'MLG':
+			print("Major League Gaming")
+	elif abbreviation == 'LOL':
+			print("Laugh Out Loud")
+	elif abbreviation == 'LIST':
+  			print("List of available abbreviations: VIP, ASAP, NASA, NASCAR, WWW, USB, RAM, CPU, SOS, EPA, LOL, VR, PTSD, CD, EST, AST, MST, ADHD, AA, NES, SNES, NDS, MB, TB, GB, GBC, ROFL, LCD, NFT, AI, WTF, MLB, MLG and LOL. More coming soon!")
+	elif abbreviation == 'Q':
+			print("Thanks for using this program!")
+			sys.exit()
+	else:
+			print("Sorry, this abbreviation is not available, or please type in caps.")
+
+  		
+# Abbreviations remaining:  GPU, PC, OS, QR, AR, NBA, ISO, 
+# , LMAO,  GIF, JPG, PNG, PST, CST, ADD, IED, DILLIGAF, BS, RC, GBA, GBC, NGC, PSX, N64, KB,  OLED, LED, RPG, JRPG, DVD, CD, VHS, ROM, C64, WOW, SMB, PDF, ORG, TY, TYSM, WDYM, WTH, CDT, DOS, OCD, TNT, LSD, DND, D&D, BBC, BP, WDW, CS, HTML, 
+# Has abbreviations of brands, image types, standard times, disabilities, text messages, computer parts, and many more! 
